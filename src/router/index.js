728a0ea3d2338c1e4e8bdc8051283af2c41b4d38
ygk103 <rladbsrms103@naver.com>
2025-09-17 14:52:38 +0900

    1

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 import FavoritesList from '../pages/FavoritesList.vue'
 
@@ -8,7 +8,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
